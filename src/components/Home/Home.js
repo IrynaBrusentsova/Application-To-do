0a3abf8e8f81f43e814aab3be6c15606b28ff791
Iryna Brusentsova <bio200480@gmail.com>
2023-03-15 20:17:10 +0200

    Remove tags from page Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,18 @@
 import SearchForm from "../SearchForm/SearchForm";
 import Hero from "../Hero/Hero.js";
 import List from "../List/List.js";
-import About from "../About/About";
-import Favorite from "../Favorite/Favorite";
+
 
 
 
 const Home = () => {
-    return(
+    return (
         <div>
             <Hero />
             <SearchForm />
             <List />
-            <About/>
-            <Favorite/>
         </div>
-        
+
     );
 };
 export default Home;
