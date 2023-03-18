c1a1f841fa558df159033150d7acf50e33ca795a
Iryna Brusentsova <bio200480@gmail.com>
2023-03-18 10:50:14 +0200

    Correct file List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -47,44 +47,4 @@ const List = () => {
 export default List;
 
 
-// ==
-// import styles from './List.module.scss';
-// import Column from '../Column/Column.js';
-// import ColumnForm from '../ColumnForm/ColumnForm';
-// import { useSelector } from 'react-redux';
-// import { getListById } from '../../redux/store';
-// import { getColumnsByList } from '../../redux/store';
-// import { useParams } from 'react-router';
-// import SearchForm from './../SearchForm/SearchForm';
 
-// const List = () => {
-
-// const { listId } = useParams();
-// //const listId = 1; 
-// const columns = useSelector(state => getColumnsByList(state, listId));
-// const listData = useSelector(state => getListById(state, listId));
-// const searchString = useSelector(state => state.searchString);
-  
-
-//   return (
-//     <div className={styles.list}>
-//       <header className={styles.header}>
-//         <h2 className={styles.title}>
-//           {listData.title}
-//         </h2>
-//       </header>
-//       <p className={styles.description}>
-//         {listData.description}
-//       </p>
-//       <SearchForm searchString={searchString}/>
-//       <section className={styles.columns}>
-//         {columns.map((column) => (
-//           <Column key={column.id} {...column} />
-//         ))}
-//       </section>
-//       <ColumnForm listId={listId} />  
-//     </div>
-//   );
-// };
-
-// export default List;
