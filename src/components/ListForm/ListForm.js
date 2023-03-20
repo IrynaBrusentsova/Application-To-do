97cc0d4b79f1f0ea7e0cc1595e426465d93f1645
Iryna Brusentsova <bio200480@gmail.com>
2023-03-20 11:12:06 +0200

    Fix file ListForm

--- src/components/ListForm/ListForm.js
+++ src/components/ListForm/ListForm.js
@@ -20,7 +20,7 @@ const ListForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(addList({ listId, description }));
+        dispatch(addList({ listId, title, description }));
         setTitle('');
         setDescription('');
     };
@@ -43,39 +43,8 @@ return (
 export default ListForm;
 
 
-// ===
-
-// import { useState } from 'react';
-// import styles from '../ColumnForm/ColumnForm.module.scss';
-// import Button from '../Button/Button';
-// import { useDispatch } from 'react-redux';
-// import {addList } from '../../redux/store';
 
 
-// const ListForm =  ({ listId }) => {
-//   const [title, setTitle] = useState('');
-//   const [description, setDescription] = useState('');
-//   const dispatch = useDispatch();
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     dispatch(addList({title, description, listId }));
-//     setTitle('');
-//     setDescription('');
-//   };
-//   return (
-//     <form className={styles.columnForm} onSubmit={handleSubmit}>
-//       Title:
-//       <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-     
-//       Description:
-//       <input type="text"value={description} onChange={(e) => setDescription(e.target.value)}/>
-
-//       <Button disabled={!title || !description}>Add Column</Button>
-//     </form>
-//   );
-// };
-// export default ListForm;
 
 
 
